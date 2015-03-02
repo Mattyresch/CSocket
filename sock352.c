bf1bfbbe6d28ca9b1ee2d757c6f9020eee382875
mattyresch <mar457@eden.rutgers.edu>
2015-03-01 21:51:28 -0500

    Worked some more on functions for sockets

--- sock352.c
+++ sock352.c
@@ -10,6 +10,11 @@
 #include <string.h>
 #include <strings.h>
 #include <pthread.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/time.h>
+#include <openssl/md5.h>
 #include "sock352.h"
 
 sock352_pkt_hdr_t *init_header(uint32_t client, in_port_t dest){
@@ -30,26 +35,63 @@ sock352_pkt_hdr_t *init_header(uint32_t client, in_port_t dest){
 	return connection;
 }
 
+
+/*	
+ *	This function takes an UDP port to connect to, and initializes a socket for
+ *	that port number
+ *
+ *	Input: int
+ *	Output: int
+ */
 int sock352_init(int udp_port){
 	if(udp_port == -1){
 		return 0;
-	}else if(udp_port == 0){
-		return SOCK352_DEFAULT_UDP_PORT;
+	}else if (udp_port != 0){
+		return SOCK352_FAILURE;
 	}else{
-		if(udp_port < 0){
-			return -1;
-		}
-		return udp_port;
+		conns = malloc(sizeof(sock352_global_t));
+		conns->active_connections = (sock352_connection_t *) NULL;
+		conns->sock352_base_fd = 0;
+		conns->sock352_recv_port = SOCK352_DEFAULT_UDP_PORT;
+		return SOCK352_SUCCESS;
 	}
 }
 
 int sock352_init2(int remote_port, int local_port){
 
 }
+/*
+ * This function takes three arguments, the domain, type, and protocol for
+ * a socket. The output is either a number indicating success, or one
+ * indicating failure. The new connection is also added to a global hashtable
+ * of all active connections, which is held within a global data structure.
+ *
+ * Input: int, int, int
+ * Output: int
+ *
+ *
+ */
 
 int sock352_socket(int domain, int type, int protocol){
 
+	//Check to make sure the domains and types are correct
+	if(domain != AF_CS352 || type != SOCK_STREAM)
+		return SOCK352_FAILURE;
+	//set up the socket with the given domain type and protocl
+	int sock = socket(domain, type, protocol);
+	//allocate memory for the connection
+	sock352_connection_t * conn = malloc(sizeof(sock352_connection_t));
+	//set up the connection
+	memset(conn, 0, sizeof(sock352_connection_t));
+	conn->state = CLOSED;
+	conn->sock352_fd = sock;
+	//add the connection to the active connections
+	HASH_ADD_INT(conns->active_connections, sock352_fd, conn);
+	//return the socket
+	return sock;
 }
+
+
 
 int sock352_bind(int fd, sockaddr_sock352_t *addr, socklen_t len){
 
